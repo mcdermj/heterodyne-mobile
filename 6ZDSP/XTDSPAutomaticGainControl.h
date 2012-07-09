41bc2f0da1f257a1af94240d63db617c9b3d1725
Jeremy McDermond <mcdermj@xenotropic.com>
2012-07-09 10:51:37 -0700

    First cut at AGC.  Needs optimization.

---
+++ 6ZDSP/XTDSPAutomaticGainControl.h
@@ -0,0 +1,13 @@
+//
+//  XTDSPAutomaticGainControl.h
+//  Heterodyne
+//
+//  Created by Jeremy McDermond on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "XTDSPModule.h"
+
+@interface XTDSPAutomaticGainControl : XTDSPModule
+
+@end
---
+++ 6ZDSP/XTDSPAutomaticGainControl.m
@@ -0,0 +1,13 @@
+//
+//  XTDSPAutomaticGainControl.m
+//  Heterodyne
+//
+//  Created by Jeremy McDermond on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "XTDSPAutomaticGainControl.h"
+
+@implementation XTDSPAutomaticGainControl
+
+@end
