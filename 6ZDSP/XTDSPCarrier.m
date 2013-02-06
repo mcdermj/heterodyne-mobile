37ef4b83b6f8baa2c692b0857dc59336886d4be7
Jeremy McDermond <mcdermj@xenotropic.com>
2013-02-06 10:02:33 -0800

    Test Carrier Module
    Created a module to generate a test carrier.
    
    Signed-off-by: Jeremy McDermond <mcdermj@xenotropic.com>

--- 6ZDSP/XTDSPCarrier.m
+++ 6ZDSP/XTDSPCarrier.m
@@ -7,7 +7,15 @@
 //
 
 #import "XTDSPCarrier.h"
+#import "XTDSPBlock.h"
 
 @implementation XTDSPCarrier
+
+-(void)performWithComplexSignal:(XTDSPBlock *)signal {
+    for(int i = 0; i < signal.blockSize; ++i) {
+        signal.realElements[i] = 0.5;
+        signal.imaginaryElements[i] = 0.0;
+    }
+}
 
 @end
